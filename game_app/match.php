426a2390da974be1a1c949becf860f869a3aa23b
MitcovaKate <katya.mitkova@bk.ru>
2024-05-08 23:15:51 +0300

    ропр

---
+++ game_app/match.php
@@ -0,0 +1,19 @@
+<?
+if(empty($_GET) && empty($_POST)){
+    print("1");
+}
+else{print("2");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
